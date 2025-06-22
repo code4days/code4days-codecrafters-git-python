097ab138aeda50de9d4a03c4f0f16310341c7782
Rasheed <relsaleh@gmail.com>
2025-06-22 15:02:49 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 
 def main():
@@ -23,6 +24,25 @@ def main():
             with open(f".git/objects/{file_hash[:2]}/{file_hash[2:]}", "rb") as f:
                 contents = zlib.decompress(f.read())
                 print(contents.decode().split("\x00")[-1], end="")
+        else:
+            raise RuntimeError(f"Unknown option: {sys.argv[2]}")
+    elif command == "hash-object":
+        if sys.argv[2] == "-w":
+            filename = sys.argv[3]
+            file_contents = ""
+            with open(filename, "r") as f:
+                file_contents = f.read()
+
+            hash_obj = f"blob {len(file_contents)}\0{file_contents}"
+            file_hash = hashlib.sha1(hash_obj.encode()).hexdigest()
+            os.mkdir(f".git/objects/{file_hash[:2]}")
+
+            with open(f".git/objects/{file_hash[:2]}/{file_hash[2:]}", "wb") as f:
+                f.write(zlib.compress(hash_obj.encode()))
+
+            print(file_hash)
+        else:
+            raise RuntimeError(f"Unknown option: {sys.argv[2]}")
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
